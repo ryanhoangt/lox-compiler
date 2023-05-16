804335d0ba9fc0ec909f99d22f494833975b3ef8
edohoangt <descience.thh10@gmail.com>
2023-05-16 13:17:41 +0700

    define and implement stack-based vm, add arithmetic opcodes

--- c/main.c
+++ c/main.c
@@ -1,18 +1,36 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+	initVM();
+
 	Chunk chunk;
 	initChunk(&chunk);
 
 	int constIdx = addConstant(&chunk, 1.2); // add constant to value array 
 	writeChunk(&chunk, OP_CONSTANT, 123);
 	writeChunk(&chunk, constIdx, 123);
+	
+	constIdx = addConstant(&chunk, 3.4);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constIdx, 123);
+	
+	writeChunk(&chunk, OP_ADD, 123);
+
+	constIdx = addConstant(&chunk, 5.6);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constIdx, 123);
+
+	writeChunk(&chunk, OP_DIVIDE, 123);
+  	writeChunk(&chunk, OP_NEGATE, 123);
 
 	writeChunk(&chunk, OP_RETURN, 123);
 
-	disassembleChunk(&chunk, "test chunk");
+	interpret(&chunk);
+
+	freeVM();
 	freeChunk(&chunk);
 
 	return 0;
